a155cba1b753707ad99c54f75b4cd23a7c77bca5
reliabledentist <62447946+reliabledentist@users.noreply.github.com>
2020-11-07 11:49:05 +0100

    Add files via upload

---
+++ code_20201107.py
@@ -0,0 +1,577 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+
+sunshine = pd.read_csv('C:\Users\rober\OneDrive\Uni\09 TechLabs\geosolar\DMIrep16-19', delimiter=";")
+
+print(sunshine)
+
+
+# In[3]:
+
+
+import pandas as pd
+
+sunshine = pd.read_csv('C:\Users\rober\OneDrive\Uni\09 TechLabs\geosolar\DMIrep16-19', delimiter=";")
+
+print(sunshine)
+
+
+# In[4]:
+
+
+import pandas as pd
+
+sunshine = pd.read_csv(C:\Users\rober\OneDrive\Uni\09 TechLabs\geosolar\DMIrep16-19.csv, delimiter=";")
+
+print(sunshine)
+
+
+# In[5]:
+
+
+get_ipython().system('which python')
+
+import pandas as pd
+
+sunshine = pd.read_csv('\Users\rober\OneDrive\Uni\09 TechLabs\geosolar\DMIrep16-19.csv', delimiter=";")
+
+print(sunshine)
+
+
+# In[6]:
+
+
+print(5)
+
+
+# In[7]:
+
+
+import pandas as pd
+
+sunshine = pd.read_csv('\Users\rober\OneDrive\Uni\09 TechLabs\geosolar\DMIrep16-19.csv', delimiter=";")
+
+print(sunshine)
+
+
+# In[8]:
+
+
+import pandas as pd
+
+sunshine = pd.read_csv('C:\Users\rober\OneDrive\Uni\09 TechLabs\geosolar\DMIrep16-19.csv', delimiter=";")
+
+print(sunshine)
+
+
+# In[9]:
+
+
+import pandas as pd
+
+sunshine = pd.read_csv(r'C:\Users\rober\OneDrive\Uni\09 TechLabs\geosolar\DMIrep16-19.csv', delimiter=";")
+
+print(sunshine)
+
+
+# In[12]:
+
+
+import pandas as pd
+
+sunshine = pd.read_csv(r'C:\Users\rober\OneDrive\Uni\09 TechLabs\geosolar\DMIrep16-19.csv', delimiter=";")
+
+sunshine = sunshine.dropna(axis = 0)
+
+shunshine_January = sunshine.loc[:, 'timer.1':'Month']
+shunshine_January = sunshine.loc[:, 'timer.1':'Month']
+shunshine_January = sunshine.loc[:, 'timer.1':'Month']
+shunshine_January = sunshine.loc[:, 'timer.1':'Month']
+
+print(sunshine)
+
+
+# In[14]:
+
+
+import pandas as pd
+
+sunshine = pd.read_csv(r'C:\Users\rober\OneDrive\Uni\09 TechLabs\geosolar\DMIrep16-19.csv', delimiter=";")
+
+sunshine = sunshine.dropna(axis = 0)
+
+shunshine_January = sunshine.loc[:, 'timer':'Month']
+shunshine_February = sunshine.loc[:, 'timer.1':'month']
+shunshine_March = sunshine.loc[:, 'timer.2':'month.1']
+shunshine_April = sunshine.loc[:, 'timer.3':'month.2']
+shunshine_May = sunshine.loc[:, 'timer.4':'month.3']
+shunshine_June = sunshine.loc[:, 'timer.5':'month.4']
+shunshine_July = sunshine.loc[:, 'timer.6':'month.5']
+shunshine_August = sunshine.loc[:, 'timer.7':'month.6']
+shunshine_September = sunshine.loc[:, 'timer.8':'month.7']
+shunshine_October = sunshine.loc[:, 'timer.9':'month.8']
+shunshine_November = sunshine.loc[:, 'timer.10':'month.9']
+shunshine_December = sunshine.loc[:, 'timer.11':'month.10']
+
+print(sunshine_January)
+
+
+# In[15]:
+
+
+import pandas as pd
+
+sunshine = pd.read_csv(r'C:\Users\rober\OneDrive\Uni\09 TechLabs\geosolar\DMIrep16-19.csv', delimiter=";")
+
+sunshine = sunshine.dropna(axis = 0)
+
+sunshine_January = sunshine.loc[:, 'timer':'Month']
+sunshine_February = sunshine.loc[:, 'timer.1':'month']
+sunshine_March = sunshine.loc[:, 'timer.1':'Month']
+sunshine_April = sunshine.loc[:, 'timer.1':'Month']
+sunshine_May = sunshine.loc[:, 'timer.1':'Month']
+sunshine_June = sunshine.loc[:, 'timer.1':'Month']
+sunshine_July = sunshine.loc[:, 'timer.1':'Month']
+sunshine_August = sunshine.loc[:, 'timer.1':'Month']
+sunshine_September = sunshine.loc[:, 'timer.1':'Month']
+sunshine_October = sunshine.loc[:, 'timer.1':'Month']
+sunshine_November = sunshine.loc[:, 'timer.1':'Month']
+sunshine_December = sunshine.loc[:, 'timer.1':'Month']
+
+print(sunshine_January)
+
+
+# In[16]:
+
+
+import pandas as pd
+
+sunshine = pd.read_csv(r'C:\Users\rober\OneDrive\Uni\09 TechLabs\geosolar\DMIrep16-19.csv', delimiter=";")
+
+sunshine = sunshine.dropna(axis = 0)
+
+sunshine_January = sunshine.loc[:, 'timer':'Month']
+sunshine_February = sunshine.loc[:, 'timer.1':'month']
+sunshine_March = sunshine.loc[:, 'timer.1':'Month']
+sunshine_April = sunshine.loc[:, 'timer.1':'Month']
+sunshine_May = sunshine.loc[:, 'timer.1':'Month']
+sunshine_June = sunshine.loc[:, 'timer.1':'Month']
+sunshine_July = sunshine.loc[:, 'timer.1':'Month']
+sunshine_August = sunshine.loc[:, 'timer.1':'Month']
+sunshine_September = sunshine.loc[:, 'timer.1':'Month']
+sunshine_October = sunshine.loc[:, 'timer.1':'Month']
+sunshine_November = sunshine.loc[:, 'timer.1':'Month']
+sunshine_December = sunshine.loc[:, 'timer.1':'Month']
+
+print(sunshine_January)
+
+
+# In[17]:
+
+
+import pandas as pd
+
+sunshine = pd.read_csv(r'C:\Users\rober\OneDrive\Uni\09 TechLabs\geosolar\DMIrep16-19.csv', delimiter=";")
+
+sunshine = sunshine.dropna(axis = 0)
+
+sunshine_January = sunshine.loc[:, 'timer':'Month']
+sunshine_February = sunshine.loc[:, 'timer.1':'month']
+sunshine_March = sunshine.loc[:, 'timer.2':'month.1']
+sunshine_April = sunshine.loc[:, 'timer.3':'month.2']
+sunshine_May = sunshine.loc[:, 'timer.4':'month.3']
+sunshine_June = sunshine.loc[:, 'timer.5':'month.4']
+sunshine_July = sunshine.loc[:, 'timer.6':'month.5']
+sunshine_August = sunshine.loc[:, 'timer.7':'month.6']
+sunshine_September = sunshine.loc[:, 'timer.8':'month.7']
+sunshine_October = sunshine.loc[:, 'timer.9':'month.8']
+sunshine_November = sunshine.loc[:, 'timer.10':'month.9']
+sunshine_December = sunshine.loc[:, 'timer.11':'month.10']
+
+print(sunshine_January)
+
+
+# In[21]:
+
+
+import pandas as pd
+
+sunshine = pd.read_csv(r'C:\Users\rober\OneDrive\Uni\09 TechLabs\geosolar\DMIrep16-19.csv', delimiter=";")
+
+sunshine = sunshine.dropna(axis = 0)
+
+sunshine_January = sunshine.loc[:, 'timer':'Month']
+sunshine_February = sunshine.loc[:, 'timer.1':'month']
+sunshine_March = sunshine.loc[:, 'timer.2':'month.1']
+sunshine_April = sunshine.loc[:, 'timer.3':'month.2']
+sunshine_May = sunshine.loc[:, 'timer.4':'month.3']
+sunshine_June = sunshine.loc[:, 'timer.5':'month.4']
+sunshine_July = sunshine.loc[:, 'timer.6':'month.5']
+sunshine_August = sunshine.loc[:, 'timer.7':'month.6']
+sunshine_September = sunshine.loc[:, 'timer.8':'month.7']
+sunshine_October = sunshine.loc[:, 'timer.9':'month.8']
+sunshine_November = sunshine.loc[:, 'timer.10':'month.9']
+sunshine_December = sunshine.loc[:, 'timer.11':'month.10']
+
+print(sunshine_January)
+print(sunshine_January)
+print(sunshine_January)
+print(sunshine_January)
+
+
+# In[22]:
+
+
+import pandas as pd
+
+sunshine = pd.read_csv(r'C:\Users\rober\OneDrive\Uni\09 TechLabs\geosolar\DMIrep16-19.csv', delimiter=";")
+
+sunshine = sunshine.dropna(axis = 0)
+
+sunshine_Jan = sunshine.loc[:, 'timer':'Month']
+sunshine_Feb = sunshine.loc[:, 'timer.1':'month']
+sunshine_Mar = sunshine.loc[:, 'timer.2':'month.1']
+sunshine_Apr = sunshine.loc[:, 'timer.3':'month.2']
+sunshine_May = sunshine.loc[:, 'timer.4':'month.3']
+sunshine_Jun = sunshine.loc[:, 'timer.5':'month.4']
+sunshine_Jul = sunshine.loc[:, 'timer.6':'month.5']
+sunshine_Aug = sunshine.loc[:, 'timer.7':'month.6']
+sunshine_Sep = sunshine.loc[:, 'timer.8':'month.7']
+sunshine_Oct = sunshine.loc[:, 'timer.9':'month.8']
+sunshine_Nov = sunshine.loc[:, 'timer.10':'month.9']
+sunshine_Dec = sunshine.loc[:, 'timer.11':'month.10']
+
+print(sunshine_Jan)
+print(sunshine_Feb)
+print(sunshine_Mar)
+print(sunshine_Apr)
+print(sunshine_May)
+print(sunshine_Jun)
+print(sunshine_Jul)
+print(sunshine_Aug)
+print(sunshine_Sep)
+print(sunshine_Oct)
+print(sunshine_Nov)
+print(sunshine_Dec)
+
+
+# In[23]:
+
+
+import pandas as pd
+
+sunshine = pd.read_csv(r'C:\Users\rober\OneDrive\Uni\09 TechLabs\geosolar\DMIrep16-19.csv', delimiter=";")
+
+sunshine = sunshine.dropna(axis = 0)
+
+sunshine_Jan = sunshine.loc[:, 'timer':'Month']
+sunshine_Feb = sunshine.loc[:, 'timer.1':'month']
+sunshine_Mar = sunshine.loc[:, 'timer.2':'month.1']
+sunshine_Apr = sunshine.loc[:, 'timer.3':'month.2']
+sunshine_May = sunshine.loc[:, 'timer.4':'month.3']
+sunshine_Jun = sunshine.loc[:, 'timer.5':'month.4']
+sunshine_Jul = sunshine.loc[:, 'timer.6':'month.5']
+sunshine_Aug = sunshine.loc[:, 'timer.7':'month.6']
+sunshine_Sep = sunshine.loc[:, 'timer.8':'month.7']
+sunshine_Oct = sunshine.loc[:, 'timer.9':'month.8']
+sunshine_Nov = sunshine.loc[:, 'timer.10':'month.9']
+sunshine_Dec = sunshine.loc[:, 'timer.11':'month.10']
+
+print(sunshine_Jan)
+print(sunshine_Feb)
+print(sunshine_Mar)
+print(sunshine_Apr)
+print(sunshine_May)
+print(sunshine_Jun)
+print(sunshine_Jul)
+print(sunshine_Aug)
+print(sunshine_Sep)
+print(sunshine_Oct)
+print(sunshine_Nov)
+print(sunshine_Dec)
+
+
+# In[25]:
+
+
+import pandas as pd
+
+sunshine = pd.read_csv(r'C:\Users\rober\OneDrive\Uni\09 TechLabs\geosolar\DMIrep16-19.csv', delimiter=";")
+
+sunshine = sunshine.dropna(axis = 0)
+
+sunshine_Jan = sunshine.loc[:, 'timer':'Month']
+sunshine_Feb = sunshine.loc[:, 'timer.1':'month']
+sunshine_Mar = sunshine.loc[:, 'timer.2':'month.1']
+sunshine_Apr = sunshine.loc[:, 'timer.3':'month.2']
+sunshine_May = sunshine.loc[:, 'timer.4':'month.3']
+sunshine_Jun = sunshine.loc[:, 'timer.5':'month.4']
+sunshine_Jul = sunshine.loc[:, 'timer.6':'month.5']
+sunshine_Aug = sunshine.loc[:, 'timer.7':'month.6']
+sunshine_Sep = sunshine.loc[:, 'timer.8':'month.7']
+sunshine_Oct = sunshine.loc[:, 'timer.9':'month.8']
+sunshine_Nov = sunshine.loc[:, 'timer.10':'month.9']
+sunshine_Dec = sunshine.loc[:, 'timer.11':'month.10']
+
+sunshine_Jan.columns = ['timer', 'kommune', 'month']
+sunshine_Jan.columns = ['timer', 'kommune', 'month']
+sunshine_Jan.columns = ['timer', 'kommune', 'month']
+sunshine_Jan.columns = ['timer', 'kommune', 'month']
+sunshine_Jan.columns = ['timer', 'kommune', 'month']
+sunshine_Jan.columns = ['timer', 'kommune', 'month']
+sunshine_Jan.columns = ['timer', 'kommune', 'month']
+
+print(sunshine_Jan)
+print(sunshine_Feb)
+print(sunshine_Mar)
+print(sunshine_Apr)
+print(sunshine_May)
+print(sunshine_Jun)
+print(sunshine_Jul)
+print(sunshine_Aug)
+print(sunshine_Sep)
+print(sunshine_Oct)
+print(sunshine_Nov)
+print(sunshine_Dec)
+
+
+# In[27]:
+
+
+import pandas as pd
+
+sunshine = pd.read_csv(r'C:\Users\rober\OneDrive\Uni\09 TechLabs\geosolar\DMIrep16-19.csv', delimiter=";")
+
+sunshine = sunshine.dropna(axis = 0)
+
+sunshine_Jan = sunshine.loc[:, 'timer':'Month']
+sunshine_Feb = sunshine.loc[:, 'timer.1':'month']
+sunshine_Mar = sunshine.loc[:, 'timer.2':'month.1']
+sunshine_Apr = sunshine.loc[:, 'timer.3':'month.2']
+sunshine_May = sunshine.loc[:, 'timer.4':'month.3']
+sunshine_Jun = sunshine.loc[:, 'timer.5':'month.4']
+sunshine_Jul = sunshine.loc[:, 'timer.6':'month.5']
+sunshine_Aug = sunshine.loc[:, 'timer.7':'month.6']
+sunshine_Sep = sunshine.loc[:, 'timer.8':'month.7']
+sunshine_Oct = sunshine.loc[:, 'timer.9':'month.8']
+sunshine_Nov = sunshine.loc[:, 'timer.10':'month.9']
+sunshine_Dec = sunshine.loc[:, 'timer.11':'month.10']
+
+sunshine_Jan.columns = ['timer', 'kommune', 'month']
+sunshine_Feb.columns = ['timer', 'kommune', 'month']
+sunshine_Mar.columns = ['timer', 'kommune', 'month']
+sunshine_Apr.columns = ['timer', 'kommune', 'month']
+sunshine_May.columns = ['timer', 'kommune', 'month']
+sunshine_Jun.columns = ['timer', 'kommune', 'month']
+sunshine_Jul.columns = ['timer', 'kommune', 'month']
+sunshine_Aug.columns = ['timer', 'kommune', 'month']
+sunshine_Sep.columns = ['timer', 'kommune', 'month']
+sunshine_Oct.columns = ['timer', 'kommune', 'month']
+sunshine_Nov.columns = ['timer', 'kommune', 'month']
+sunshine_Dec.columns = ['timer', 'kommune', 'month']
+
+print(sunshine_Jan)
+print(sunshine_Feb)
+print(sunshine_Mar)
+print(sunshine_Apr)
+print(sunshine_May)
+print(sunshine_Jun)
+print(sunshine_Jul)
+print(sunshine_Aug)
+print(sunshine_Sep)
+print(sunshine_Oct)
+print(sunshine_Nov)
+print(sunshine_Dec)
+
+frames = [sunshine_Jan, sunshine_Feb, sunshine_Mar, sunshine_Apr, sunshine_May, sunshine_Jun, sunshine_Jul, sunshine_Aug, sunshine_Sep, sunshine_Oct, sunshine_Nov, sunshine_Dec]
+
+result = pd.concat(frames, ignore_index = True)
+
+print(result)
+
+
+# In[29]:
+
+
+import pandas as pd
+
+sunshine = pd.read_csv(r'C:\Users\rober\OneDrive\Uni\09 TechLabs\geosolar\DMIrep16-19.csv', delimiter=";")
+
+sunshine = sunshine.dropna(axis = 0)
+
+sunshine_Jan = sunshine.loc[:, 'timer':'Month']
+sunshine_Feb = sunshine.loc[:, 'timer.1':'month']
+sunshine_Mar = sunshine.loc[:, 'timer.2':'month.1']
+sunshine_Apr = sunshine.loc[:, 'timer.3':'month.2']
+sunshine_May = sunshine.loc[:, 'timer.4':'month.3']
+sunshine_Jun = sunshine.loc[:, 'timer.5':'month.4']
+sunshine_Jul = sunshine.loc[:, 'timer.6':'month.5']
+sunshine_Aug = sunshine.loc[:, 'timer.7':'month.6']
+sunshine_Sep = sunshine.loc[:, 'timer.8':'month.7']
+sunshine_Oct = sunshine.loc[:, 'timer.9':'month.8']
+sunshine_Nov = sunshine.loc[:, 'timer.10':'month.9']
+sunshine_Dec = sunshine.loc[:, 'timer.11':'month.10']
+
+sunshine_Jan.columns = ['timer', 'kommune', 'month']
+sunshine_Feb.columns = ['timer', 'kommune', 'month']
+sunshine_Mar.columns = ['timer', 'kommune', 'month']
+sunshine_Apr.columns = ['timer', 'kommune', 'month']
+sunshine_May.columns = ['timer', 'kommune', 'month']
+sunshine_Jun.columns = ['timer', 'kommune', 'month']
+sunshine_Jul.columns = ['timer', 'kommune', 'month']
+sunshine_Aug.columns = ['timer', 'kommune', 'month']
+sunshine_Sep.columns = ['timer', 'kommune', 'month']
+sunshine_Oct.columns = ['timer', 'kommune', 'month']
+sunshine_Nov.columns = ['timer', 'kommune', 'month']
+sunshine_Dec.columns = ['timer', 'kommune', 'month']
+
+    #   print(sunshine_Jan)
+    #   print(sunshine_Feb)
+    #   print(sunshine_Mar)
+    #   print(sunshine_Apr)
+    #   print(sunshine_May)
+    #   print(sunshine_Jun)
+    #   print(sunshine_Jul)
+    #   print(sunshine_Aug)
+    #   print(sunshine_Sep)
+    #   print(sunshine_Oct)
+    #   print(sunshine_Nov)
+    #   print(sunshine_Dec)
+
+frames = [sunshine_Jan, sunshine_Feb, sunshine_Mar, sunshine_Apr, sunshine_May, sunshine_Jun, sunshine_Jul, sunshine_Aug, sunshine_Sep, sunshine_Oct, sunshine_Nov, sunshine_Dec]
+
+result = pd.concat(frames, ignore_index = True)
+
+    # result = result.set_index(['kommune', 'month'])
+
+print(result)
+
+
+# In[30]:
+
+
+import pandas as pd
+
+sunshine = pd.read_csv(r'C:\Users\rober\OneDrive\Uni\09 TechLabs\geosolar\DMIrep16-19.csv', delimiter=";")
+
+sunshine = sunshine.dropna(axis = 0)
+
+sunshine_Jan = sunshine.loc[:, 'timer':'Month']
+sunshine_Feb = sunshine.loc[:, 'timer.1':'month']
+sunshine_Mar = sunshine.loc[:, 'timer.2':'month.1']
+sunshine_Apr = sunshine.loc[:, 'timer.3':'month.2']
+sunshine_May = sunshine.loc[:, 'timer.4':'month.3']
+sunshine_Jun = sunshine.loc[:, 'timer.5':'month.4']
+sunshine_Jul = sunshine.loc[:, 'timer.6':'month.5']
+sunshine_Aug = sunshine.loc[:, 'timer.7':'month.6']
+sunshine_Sep = sunshine.loc[:, 'timer.8':'month.7']
+sunshine_Oct = sunshine.loc[:, 'timer.9':'month.8']
+sunshine_Nov = sunshine.loc[:, 'timer.10':'month.9']
+sunshine_Dec = sunshine.loc[:, 'timer.11':'month.10']
+
+sunshine_Jan.columns = ['timer', 'kommune', 'month']
+sunshine_Feb.columns = ['timer', 'kommune', 'month']
+sunshine_Mar.columns = ['timer', 'kommune', 'month']
+sunshine_Apr.columns = ['timer', 'kommune', 'month']
+sunshine_May.columns = ['timer', 'kommune', 'month']
+sunshine_Jun.columns = ['timer', 'kommune', 'month']
+sunshine_Jul.columns = ['timer', 'kommune', 'month']
+sunshine_Aug.columns = ['timer', 'kommune', 'month']
+sunshine_Sep.columns = ['timer', 'kommune', 'month']
+sunshine_Oct.columns = ['timer', 'kommune', 'month']
+sunshine_Nov.columns = ['timer', 'kommune', 'month']
+sunshine_Dec.columns = ['timer', 'kommune', 'month']
+
+    #   print(sunshine_Jan)
+    #   print(sunshine_Feb)
+    #   print(sunshine_Mar)
+    #   print(sunshine_Apr)
+    #   print(sunshine_May)
+    #   print(sunshine_Jun)
+    #   print(sunshine_Jul)
+    #   print(sunshine_Aug)
+    #   print(sunshine_Sep)
+    #   print(sunshine_Oct)
+    #   print(sunshine_Nov)
+    #   print(sunshine_Dec)
+
+frames = [sunshine_Jan, sunshine_Feb, sunshine_Mar, sunshine_Apr, sunshine_May, sunshine_Jun, sunshine_Jul, sunshine_Aug, sunshine_Sep, sunshine_Oct, sunshine_Nov, sunshine_Dec]
+
+result = pd.concat(frames, ignore_index = True)
+
+    # result = result.set_index(['kommune', 'month'])
+
+result = result.sort_index('kommune', 'month')
+
+print(result)
+
+
+# In[31]:
+
+
+import pandas as pd
+
+sunshine = pd.read_csv(r'C:\Users\rober\OneDrive\Uni\09 TechLabs\geosolar\DMIrep16-19.csv', delimiter=";")
+
+sunshine = sunshine.dropna(axis = 0)
+
+sunshine_Jan = sunshine.loc[:, 'timer':'Month']
+sunshine_Feb = sunshine.loc[:, 'timer.1':'month']
+sunshine_Mar = sunshine.loc[:, 'timer.2':'month.1']
+sunshine_Apr = sunshine.loc[:, 'timer.3':'month.2']
+sunshine_May = sunshine.loc[:, 'timer.4':'month.3']
+sunshine_Jun = sunshine.loc[:, 'timer.5':'month.4']
+sunshine_Jul = sunshine.loc[:, 'timer.6':'month.5']
+sunshine_Aug = sunshine.loc[:, 'timer.7':'month.6']
+sunshine_Sep = sunshine.loc[:, 'timer.8':'month.7']
+sunshine_Oct = sunshine.loc[:, 'timer.9':'month.8']
+sunshine_Nov = sunshine.loc[:, 'timer.10':'month.9']
+sunshine_Dec = sunshine.loc[:, 'timer.11':'month.10']
+
+sunshine_Jan.columns = ['timer', 'kommune', 'month']
+sunshine_Feb.columns = ['timer', 'kommune', 'month']
+sunshine_Mar.columns = ['timer', 'kommune', 'month']
+sunshine_Apr.columns = ['timer', 'kommune', 'month']
+sunshine_May.columns = ['timer', 'kommune', 'month']
+sunshine_Jun.columns = ['timer', 'kommune', 'month']
+sunshine_Jul.columns = ['timer', 'kommune', 'month']
+sunshine_Aug.columns = ['timer', 'kommune', 'month']
+sunshine_Sep.columns = ['timer', 'kommune', 'month']
+sunshine_Oct.columns = ['timer', 'kommune', 'month']
+sunshine_Nov.columns = ['timer', 'kommune', 'month']
+sunshine_Dec.columns = ['timer', 'kommune', 'month']
+
+    #   print(sunshine_Jan)
+    #   print(sunshine_Feb)
+    #   print(sunshine_Mar)
+    #   print(sunshine_Apr)
+    #   print(sunshine_May)
+    #   print(sunshine_Jun)
+    #   print(sunshine_Jul)
+    #   print(sunshine_Aug)
+    #   print(sunshine_Sep)
+    #   print(sunshine_Oct)
+    #   print(sunshine_Nov)
+    #   print(sunshine_Dec)
+
+frames = [sunshine_Jan, sunshine_Feb, sunshine_Mar, sunshine_Apr, sunshine_May, sunshine_Jun, sunshine_Jul, sunshine_Aug, sunshine_Sep, sunshine_Oct, sunshine_Nov, sunshine_Dec]
+
+result = pd.concat(frames, ignore_index = True)
+
+    # result = result.set_index(['kommune', 'month'])
+
+result = result.sort_index('kommune', 'month')
+
+print(result)
+
+
+# In[ ]:
+
+
+
+
