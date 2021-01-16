06bac358cb3d4ae270bc9f01486db6462705c79b
Clara Neu <mariaclara.neu@gmail.com>
2021-01-16 16:58:05 +0100

    test af cors 3

--- html/scripts/calculator.js
+++ html/scripts/calculator.js
@@ -12,7 +12,7 @@ $("#user-input").click(function(){
 
     fetch('https://geosolarapi.azurewebsites.net/api/HttpTrigger3?municipalities=test&energy=test&household=test')
     .then(response => response.json()) //these two are promises, giving asynchronous behavior, so that the promise first gets resolved when
-    .then(data => alert(data)); //The fetch returns a promise (line 13), and we already know its a json file ()
+    .then(data => alert(data.Hello)); //The fetch returns a promise (line 13), and we already know its a json file ()
 
   });
 
